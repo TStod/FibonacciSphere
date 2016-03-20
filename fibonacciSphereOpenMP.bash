614548f894ffd1b3832c453d56d45e7194df9389
TStod <stoddard.t@husky.neu.edu>
2016-03-20 17:16:08 -0400

    Adding OpenMP starting files

---
+++ fibonacciSphereOpenMP.bash
@@ -0,0 +1,11 @@
+#!/bin/sh
+#BSUB -J Teddy-8c
+#BSUB -o project_output_file
+#BSUB -e project_error_file
+#BSUB -n 1
+#BSUB -q ht-10g
+#BSUB cwd /home/stoddard.t/FibonacciSphere/
+work=/home/stoddard.t/FibonacciSphere/
+
+cd $work
+bin/fibonacciSphere 100000 1000000 123456789
